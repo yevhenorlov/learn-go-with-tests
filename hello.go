457b682a80d3bde2c4cf850395e3fa2eed956f7c
yevhen orlov <yevhenorlov@gmail.com>
2024-08-31 18:54:01 +0200

    fallback value

--- hello.go
+++ hello.go
@@ -5,9 +5,12 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
-  return englishHelloPrefix + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
-  fmt.Println(Hello("world"))
+	fmt.Println(Hello("world"))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-  got := Hello("Chris")
-  want := "Hello, Chris"
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assert(t, got, want)
+	})
 
-  if got != want {
-    t.Errorf("got %q want %q", got, want)
-  }
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assert(t, got, want)
+	})
+}
+
+func assert(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
