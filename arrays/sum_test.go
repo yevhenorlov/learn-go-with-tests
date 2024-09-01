2ade5b56925a562ad7d68ab600a0ceb2d980f7ac
yevhen orlov <yevhenorlov@gmail.com>
2024-09-01 12:04:34 +0200

    sum all

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
@@ -11,6 +14,17 @@ func TestSum(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("passes", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
 	})
 }
