6246473f467564bca02d47ee2e5d8cb181807f60
yevhen orlov <yevhenorlov@gmail.com>
2024-09-01 12:17:33 +0200

    sum all tails

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -19,12 +19,19 @@ func TestSum(t *testing.T) {
 }
 
 func TestSumAll(t *testing.T) {
-	t.Run("passes", func(t *testing.T) {
-		got := SumAll([]int{1, 2}, []int{0, 9})
-		want := []int{3, 9}
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
 
-		if !slices.Equal(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
-	})
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
